cb6096dd99b45dc0a21779ca4a9fdaf8d6be1ece
Michael Schlaepfer <michael.schlaepfer@adlos.com>
2020-10-07 13:08:09 +0200

    Neue LoadingControls eingeführt. Sind noch nicht ausprogrammiert!

---
+++ CommonControlsLibSolution/OperatingControlsLib/Windows10LoadingControl.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OperatingControlsLib
+{
+    /// <summary>
+    /// Interaktionslogik für Windows10LoadingControl.xaml
+    /// </summary>
+    public partial class Windows10LoadingControl : UserControl
+    {
+        public Windows10LoadingControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
